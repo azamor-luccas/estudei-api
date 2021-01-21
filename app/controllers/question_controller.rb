caad5c0648215f442db502135bbfe33422f1efb7
Azamor <luccas.azamor@gmail.com>
2021-01-20 23:32:49 -0300

    Created routes and controller

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,46 @@
+class QuestionController < ApplicationController
+    def index
+    end
+
+    def create
+        question = Question.new(create_question_params)
+        if question.save
+            alternatives = []
+            params['alternatives'].each do |alternative|
+                alternatives.append({
+                    "question_id": question.id,
+                    "letter": alternative[0],
+                    "description": alternative[1],
+                    "correct": params['correctAlternative'] == alternative[0],
+                    "created_at": Time.now,
+                    "updated_at": Time.now,
+                })
+            end
+
+            if QuestionAlternative.insert_all(alternatives)
+                render json: question, status: :ok
+            else
+                render json: question.errors, status: :unprocessable_entity
+            end
+
+        else
+            render json: question.errors, status: :unprocessable_entity
+        end
+
+    end
+
+    private
+    def create_question_params
+        params.permit(
+            :statement,
+            :subject,
+        )
+    end
+
+    def create_question_alternative_params
+        params.permit(
+            :description,
+            :correct,
+        )
+    end
+end
