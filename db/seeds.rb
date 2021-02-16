9905069fcd23a067e01a9620ea67ffc02292ff82
Azamor <luccas.azamor@gmail.com>
2021-02-15 21:21:36 -0300

    Added db_populate:create_subjecs taks to db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Rake::Task['db_populate:create_subjects'].invoke
+
